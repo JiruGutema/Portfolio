155ef39b358f0a9a26fca708285320a1ebf373ab
JiruGutema <jethior1@gmail.com>
2024-10-31 16:33:37 +0300

    portfolio section is added

---
+++ js/darkMode.js
@@ -0,0 +1,9 @@
+// const button = document.getElementById("toggleButton");
+// if (button) {
+//   button.addEventListener("click", () => {
+//     const body = document.body;
+//     body.style.backgroundColor =
+//       body.style.backgroundColor === "black" ? "white" : "#333333";
+//     body.style.color = body.style.color === "white" ? "gray" : "white";
+//   });
+// }
